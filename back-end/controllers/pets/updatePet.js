1548433b6474403a7aa5bfa1f97b077e47db68e3
felipemacedo315 <felipemacedo315@gmail.com>
2022-06-14 11:59:23 -0300

    Upload de imagens do pet funcionando

--- back-end/controllers/pets/updatePet.js
+++ back-end/controllers/pets/updatePet.js
@@ -1,7 +1,14 @@
 const petsModel = require("../../models/pets");
+
 async function updatePet(req, res) {
   const exactPetId = req.headers.idpet;
   const { animalPet, corPet, sexoPet, pesoPet } = req.body;
+
+  const imagensPet = req.files.map((item) => {
+    return item.originalname;
+  });
+  console.log(imagensPet);
+
   const updateInfoPet = await petsModel.updateOne(
     { _id: exactPetId },
     {
@@ -9,9 +16,10 @@ async function updatePet(req, res) {
       cor: corPet,
       sexo: sexoPet,
       peso: pesoPet,
+      imagens: imagensPet,
     }
   );
-  res.send(updateInfoPet);
+  res.send(req.files);
 }
 
 module.exports = updatePet;
--- back-end/routes/petsRoutes.js/updatePetRoute.js
+++ back-end/routes/petsRoutes.js/updatePetRoute.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const updatePet = require('../../controllers/pets/updatePet')
-const updatePetRoute = express.Router()
+const express = require("express");
+const updatePet = require("../../controllers/pets/updatePet");
+const multer = require("multer");
+const upload = multer({ dest: "petImagens" });
+const updatePetRoute = express.Router();
 
-updatePetRoute.patch('/updatePet',updatePet)
+updatePetRoute.patch("/updatePet", upload.array("fotopet", 8), updatePet);
 
-module.exports = updatePetRoute
+module.exports = updatePetRoute;
